3deeceb98dfccb16a8c5085624ed899fda60b760
Ross Buchan <rossbuchan1@googlemail.com>
2023-08-08 15:29:52 +0100

    Add method to paintCan so it can check if its empty

--- models/paintCan.js
+++ models/paintCan.js
@@ -2,4 +2,13 @@ const PaintCan = function (volume) {
     this.volume = volume;
 };
 
+PaintCan.prototype.checkEmpty = function() {
+    if (this.volume == 0) {
+        return true;
+    }
+    else {
+        return false;
+    };
+};
+
 module.exports = PaintCan
--- models/specs/paintCan_spec.js
+++ models/specs/paintCan_spec.js
@@ -6,9 +6,15 @@ describe('paintCan', function() {
 
     this.beforeEach(function() {
         paintCan = new PaintCan(5);
-    })
+    });
+
     it('should have a volume', function() {
         const actual = paintCan.volume;
         assert.strictEqual(actual, 5);
     });
+
+    it('should be able to check if its empty', function() {
+        const actual = paintCan.checkEmpty();
+        assert.strictEqual(actual, false);
+    })
 });
